e2e0318403f3da221a61e8fb7f40b32c02bed775
Iliyas Robin <iliyasahmedr@gmail.com>
2024-12-02 19:43:14 +0600

    Update userInput.dart

--- userInput.dart
+++ userInput.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 void main(){
   print("enter your name");
   String ? name = stdin.readLineSync()!;
-  print("enter your age");
-  int ? age = int.parse(stdin.readLineSync()!);
+  print("enter your age $name");
+  int age = int.parse(stdin.readLineSync()!);
   print(name);
   print(age);
 
