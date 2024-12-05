535aeac132e166adfc41cb5359d8f24e35bd6e80
Iliyas Robin <iliyasahmedr@gmail.com>
2024-12-05 19:23:59 +0600

    Update mapExtended.dart

--- mapExtended.dart
+++ mapExtended.dart
@@ -15,3 +15,29 @@ void main(){
 
 var newMap = Map.of(per);
 print(newMap)
+
+
+   //extended
+
+   var person= {
+    'name': 'iliyas',
+    'age': '25',
+    'skill':'flutter,Dart',
+    "experience":'new'
+  };
+  var person2= {
+    'name': 'iliyas',
+    'age': '25',
+    "experience":'new'
+  };
+
+  print(person == person2);
+
+  print(person.containsKey('name'));
+  print(person.containsValue('rahman'));
+
+  print(person.keys);
+  print(person.values);
+
+  var person3 = Map.of(person2);
+  print('person3: $person3');
