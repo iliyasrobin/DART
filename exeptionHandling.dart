a0c380ee48a9a86d4b92c330e41cd8526380a9df
Iliyas Robin <iliyasahmedr@gmail.com>
2024-12-25 20:29:37 +0600

    exeptionHandling.dart

--- exeptionHandling.dart
+++ exeptionHandling.dart
@@ -1,1 +1,41 @@
 //code
+
+ try{
+    List<int> numbers = [10,20,30];
+    print(numbers[5]);
+  }catch(e){
+    print("List e nei ");
+  }
+
+
+  try{
+  int num = 5;
+
+  if(num == 5){
+    throw Exception('Can not be a neg number');
+  }else{
+    print("Okay perfect");
+  }
+  }catch(e){
+    print(e);
+  }
+
+
+  try{
+    String ? name;
+    print(name!.length);
+  }catch(e){
+    print(e);
+
+  }
+
+
+  try{
+   int num = int.parse("abc");
+   print(num);
+  } on FormatException catch(e){
+    print("Format Ex");
+  }catch(e){
+    print("Normal catch ");
+
+  }
