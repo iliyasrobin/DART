31ca48c9324ad9be2619f3d5b0d6e33012251d5a
Iliyas Robin <iliyasahmedr@gmail.com>
2024-12-25 19:23:55 +0600

    Update tryCatch.dart

--- tryCatch.dart
+++ tryCatch.dart
@@ -15,3 +15,20 @@ main(){
 
 
 //updating 
+
+
+main() {
+  try {
+    String age = "20";
+    int age_int = int.parse(age);
+    if (age_int > 18) {
+      print("able to vote");
+    } else {
+      print("Unable to vote");
+    }
+  } catch (e) {
+    print("tmr vule celo ekh e : $e");
+  }finally{
+    print("I'm from finally state");
+  }
+}
