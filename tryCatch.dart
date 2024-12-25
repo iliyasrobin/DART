c90b83625aeca88deda72c16bc97ce7fcc66c219
Iliyas Robin <iliyasahmedr@gmail.com>
2024-12-25 19:36:48 +0600

    Update tryCatch.dart

--- tryCatch.dart
+++ tryCatch.dart
@@ -32,3 +32,46 @@ main() {
     print("I'm from finally state");
   }
 }
+
+
+//more
+
+ try{
+    List<int> numbers = [10,20,30];
+    print(numbers[5]);
+  }catch(e){
+    print("List e nei ");
+  }
+
+
+  try{
+  int num = 5;
+
+  if(num == 5){
+    throw Exception('Can not be a neg number');
+  }else{
+    print("Okay perfect");
+  }
+  }catch(e){
+    print(e);
+  }
+
+
+  try{
+    String ? name;
+    print(name!.length);
+  }catch(e){
+    print(e);
+
+  }
+
+
+  try{
+   int num = int.parse("abc");
+   print(num);
+  } on FormatException catch(e){
+    print("Format Ex");
+  }catch(e){
+    print("Normal catch ");
+
+  }
